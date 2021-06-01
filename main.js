fbf242e00d72d5999725f21e4aeb0b9630b06167
Siddhant-2011 <74121489+Siddhant-2011@users.noreply.github.com>
2021-06-01 08:52:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var prediction1="";
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+var camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="final" src="'+data_uri+'"/>';
+    });
+}
+  console.log(ml5.version);
+  var classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/IGZvUeqAX/model.json",loaded);
+  function loaded(){
+      console.log("model has been loaded")
+  }
+  function speak(){
+    var synph=window.speechSynthesis;
+    var speak="The Prediction Is"+prediction1;
+    var utterthis=new SpeechSynthesisUtterance(speak);
+    synph.speak(utterthis);
+    }
+    function check(){
+     var store=document.getElementById("final");
+     classifier.classify(store, gotresult);
+    }
+function gotresult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_hand_name").innerHTML=results[0].label;
+        prediction1=results[0].label;
+        speak();
+        if(results[0].label=="Sad"){
+            document.getElementById("update_hand").innerHTML="&#128546;";
+        }
+        if(results[0].label=="Angry"){
+          document.getElementById("update_hand").innerHTML="&#128545;";
+      }
+      if(results[0].label=="Happy"){
+          document.getElementById("update_hand").innerHTML="&#128522;";
+      }
+    }
+ 
+}
